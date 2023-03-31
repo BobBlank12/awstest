5c5ca5e7dc8d392900c19f1991609e9cd7155aeb
BobBlank12 <39975390+BobBlank12@users.noreply.github.com>
2023-03-31 16:24:25 -0500

    finished py code using boto3 to create VPC

--- build_aws_with_boto3.py
+++ build_aws_with_boto3.py
@@ -4,8 +4,6 @@ from botocore.config import Config
 import time
 from jsonpath_ng import jsonpath, parse
 
-### Functions ###
-
 
 ### Main ###
 os.system("clear")
@@ -54,9 +52,9 @@ while (vpc_state != "available") and (x < 10):
     )
     jsonpath_expression = parse('Vpcs[*].State')
     for match in jsonpath_expression.find(response):
-       vpc_state = match.value
-    x = x + 1
-    time.sleep(1)
+        vpc_state = match.value
+        x = x + 1
+        time.sleep(1)
 
 if (x > 10) or (vpc_state != "available"):
     print("VPC is not available after 10 seconds... aborting.")
@@ -66,10 +64,228 @@ else:
     print(f"Vpc Id: {vpc_id} is available.")
 
 #Describe the main route table of the new VPC so we can get its ID and tag it with a name
-#aws ec2 describe-route-tables --filters "Name=association.main,Values=true" "Name=vpc-id,Values=vpc-02645ba38f23e937d"
+response = client.describe_route_tables(
+    Filters=[
+        {
+            'Name': 'association.main',
+            'Values': [
+                'true'
+            ]
+        },
+        {
+            'Name': 'vpc-id',
+            'Values': [
+                vpc_id
+            ]
+        }
+    ],
+    DryRun=False
+)
+
+jsonpath_expression = parse('RouteTables[*].RouteTableId')
+for match in jsonpath_expression.find(response):
+    routetableid = match.value
+print(f"routetableid={routetableid}")
+    
+#Add a name tag to the default main route table for the VPC
+response = client.create_tags(
+    DryRun=False,
+    Resources=[
+        routetableid,
+    ],
+    Tags=[
+        {
+            'Key': 'Name',
+            'Value': vpc_name_prefix + "-rtb-main"
+        },
+    ]
+)
+
+# Enable dns-hostnames on the new VPC
+response = client.modify_vpc_attribute(
+    EnableDnsHostnames={
+        'Value': True
+    },
+    VpcId=vpc_id
+)
+
+#Create the S3 Endpoint Gateway
+response = client.create_vpc_endpoint(
+    DryRun=False,
+    VpcEndpointType='Gateway',
+    VpcId=vpc_id,
+    ServiceName='com.amazonaws.us-east-2.s3',
+    TagSpecifications=[
+        {
+            'ResourceType': 'vpc-endpoint',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + "-vpce-s3"
+                }
+            ]
+        }
+    ]
+)
+
+#Capture the new S3 Endpoint ID
+jsonpath_expression = parse('VpcEndpoint[*].VpcEndpointId')
+for match in jsonpath_expression.find(response):
+    s3_endpoint_id = match.value
+print(f"s3_endpoint_id={s3_endpoint_id}")
+
+#Subnet creation call - this will be my public subnet
+response = client.create_subnet(
+    TagSpecifications=[
+        {
+            'ResourceType': 'subnet',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + '-subnet-public1-us-east-2a'
+                }
+            ]
+        }
+    ],
+    CidrBlock='11.0.0.0/20',
+    VpcId=vpc_id,
+    DryRun=False
+)
+
+#Capture the new public subnet ID
+jsonpath_expression = parse('Subnet[*].SubnetId')
+for match in jsonpath_expression.find(response):
+    pub_subnet_id = match.value
+print (f"pub_subnet_id={pub_subnet_id}")
+
+#Subnet creation call - this will be my private subnet
+response = client.create_subnet(
+    TagSpecifications=[
+        {
+            'ResourceType': 'subnet',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + '-subnet-private1-us-east-2a'
+                }
+            ]
+        }
+    ],
+    CidrBlock='11.0.128.0/20',
+    VpcId=vpc_id,
+    DryRun=False
+)
+
+#Capture the new private subnet ID
+jsonpath_expression = parse('Subnet[*].SubnetId')
+for match in jsonpath_expression.find(response):
+    private_subnet_id = match.value
+print (f"private_subnet_id={private_subnet_id}")
+
+#Create the internet gateway
+response = client.create_internet_gateway(
+    TagSpecifications=[
+        {
+            'ResourceType': 'internet-gateway',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + '-igw'
+                }
+            ]
+        }
+    ],
+    DryRun=False
+)
+
+# Capture the new IGW ID
+jsonpath_expression = parse('InternetGateway[*].InternetGatewayId')
+for match in jsonpath_expression.find(response):
+    igw_id = match.value
+print (f"igw_id={igw_id}")
+
+# Attach the internet gateway IGW to the VPC
+response = client.attach_internet_gateway(
+    DryRun=False,
+    InternetGatewayId=igw_id,
+    VpcId=vpc_id
+)
+
+#Create a route table to be used by the public subnet
+response = client.create_route_table(
+    DryRun=False,
+    VpcId=vpc_id,
+    TagSpecifications=[
+        {
+            'ResourceType': 'route-table',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + '-rtb-public'
+                }
+            ]
+        }
+    ]
+)
+
+# Capture the new pub route table ID
+jsonpath_expression = parse('RouteTable[*].RouteTableId')
+for match in jsonpath_expression.find(response):
+    pub_route_table_id = match.value
+print (f"pub_route_table_id={pub_route_table_id}")
 
 
+# Create (add) the route to the route table for all traffic to go to the IGW
+response = client.create_route(
+    DestinationCidrBlock='0.0.0.0/0',
+    DryRun=False,
+    GatewayId=igw_id,
+    RouteTableId=pub_route_table_id
+)
 
+#Associate the route with the public subnet
+response = client.associate_route_table(
+    DryRun=False,
+    RouteTableId=pub_route_table_id,
+    SubnetId=pub_subnet_id
+)
 
+#Create a route table to be used by the private subnet
+response = client.create_route_table(
+    DryRun=False,
+    VpcId=vpc_id,
+    TagSpecifications=[
+        {
+            'ResourceType': 'route-table',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': vpc_name_prefix + '-rtb-private1-us-east-2a'
+                }
+            ]
+        }
+    ]
+)
 
+# Capture the new pub route table ID
+jsonpath_expression = parse('RouteTable[*].RouteTableId')
+for match in jsonpath_expression.find(response):
+    private_route_table_id = match.value
+print (f"private_route_table_id={private_route_table_id}")
 
+#Associate the route with the private subnet
+response = client.associate_route_table(
+    DryRun=False,
+    RouteTableId=private_route_table_id,
+    SubnetId=private_subnet_id
+)
+
+#Associate the S3 Endpoint with the private subnet route table (the reset policy was just in the example so I kept it.)
+response = client.modify_vpc_endpoint(
+    DryRun=False,
+    VpcEndpointId=s3_endpoint_id,
+    ResetPolicy=True,
+    AddRouteTableIds=[
+        private_route_table_id,
+    ]
+)
